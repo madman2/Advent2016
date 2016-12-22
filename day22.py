afd376e9984c2d30b7e84bd7dc0d9e183c718c86
Matthew Dorsett <matthew.a.dorsett@gmail.com>
2016-12-22 10:40:30 -0800

    Day 22 uses regex now

--- day22.py
+++ day22.py
@@ -1,23 +1,32 @@
 # Day 22: Grid Computing
 
 from itertools import permutations
+import re
 
 def main():
     with open('day22.txt') as f:
         lines = f.read().strip().split('\n')
 
     lines = lines[2:]
-    nodes = []
-    for line in lines:
-        words = line.split()
-        nodes.append((int(words[2][:len(words[2]) - 1]), int(words[3][:len(words[3]) - 1])))
+    grid = build_grid(lines)
 
     viable_nodes = 0
-    for node1, node2 in permutations(nodes, 2):
+    for node1, node2 in permutations(grid, 2):
         if node2[1] >= node1[0] and node1[0] != 0:
             viable_nodes += 1
 
-    print(viable_nodes)
+    print("Part 1: {}".format(viable_nodes))
+
+def build_grid(lines):
+    get_numbers = re.compile('(\d+)T')
+    grid = [0 for i in range(0, len(lines))]
+    for idx, line in enumerate(lines):
+        grid[idx] = tuple(map(int, re.findall(get_numbers, line)[1:]))
+
+    return grid
+
+def solve():
+    pass
 
 
 if __name__ == "__main__":
